5bac13cb6f70a9bbec5c9ea35396fa30a6355561
chinococo <garyliuyahoocom384@gmail.com>
2020-01-09 22:27:20 +0800

    test for ouput file

--- app/src/main/java/com/example/myapplication/wirte_data.java
+++ app/src/main/java/com/example/myapplication/wirte_data.java
@@ -7,31 +7,51 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 
 public class wirte_data extends Thread {
     Context context;
-    wirte_data(Context context)
+    ArrayList<score_struct> data;
+    File floder;
+    String name;
+    wirte_data(Context context, ArrayList<score_struct> i,File floder,String name)
     {
         this.context=context;
+        this.data=i;
+        this.floder=floder;
+        this.name=name;
     }
 
     @Override
     public void run() {
         super.run();
-        Log.e("123","123");
-        File sdcard= Environment.getExternalStorageDirectory();
+        File sdcard= floder;
             try
             {
                 if(!sdcard.exists())
                 sdcard.mkdirs();
-                File output=new File(sdcard,"ouput.txt");
+                File output=new File(sdcard,name+".csv");
                 FileOutputStream outputStream=new FileOutputStream(output);
-                if(!output.exists()) output.createNewFile();
-                outputStream.write(123);
+                if(!output.exists())
+                output.createNewFile();
+                StringBuilder ouputdata=new StringBuilder();
+                for(int k=0;k<data.size();k++)
+                {
+                    ouputdata.append(data.get(k).name+",");
+                    for(int g=0;g<data.get(k).score.size();g++)
+                    {
+                        ouputdata.append(data.get(k).score.get(g));
+                        if(g==data.get(k).score.size()-1)
+                            ouputdata.append("\n");
+                        else
+                            ouputdata.append(",");
+                    }
+                }
+                outputStream.write(ouputdata.toString().getBytes());
                 outputStream.flush();
                 outputStream.close();
-                Toast.makeText(context, "輸出完成", Toast.LENGTH_LONG).show();
-                Log.e("123","123");
+                Toast.makeText(context, ""+floder, Toast.LENGTH_LONG).show();
+                //Log.e("123","123");
             }
             catch (Exception e)
             {
